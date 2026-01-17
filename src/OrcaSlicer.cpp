32cda169b7f25727d44661817ffcd4bd430ea43e
Neo <156867325+neo-jayfeather@users.noreply.github.com>
2026-01-17 09:58:08 -0500

    Revert "complete removal of boostfs and replacement with stdfs"
    This reverts commit 33d45145e583be6a578f4a4995a11aa84fdbafb4.

--- src/OrcaSlicer.cpp
+++ src/OrcaSlicer.cpp
@@ -37,6 +37,7 @@ using namespace nlohmann;
 #endif
 
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/nowide/args.hpp>
 #include <boost/nowide/cstdlib.hpp>
 #include <boost/nowide/iostream.hpp>
@@ -461,11 +462,11 @@ static int decode_png_to_thumbnail(std::string png_file, ThumbnailData& thumbnai
         return -1;
     }
 
-    const std::size_t &size  = stdfs::file_size(png_file);
+    const std::size_t &size  = boost::filesystem::file_size(png_file);
     std::string png_buffer(size, '\0');
     png_buffer.reserve(size);
 
-    stdfs::ifstream ifs(png_file, std::ios::binary);
+    boost::filesystem::ifstream ifs(png_file, std::ios::binary);
     ifs.read(png_buffer.data(), png_buffer.size());
     ifs.close();
 
@@ -1193,7 +1194,7 @@ int CLI::run(int argc, char **argv)
     XInitThreads();
 #endif
 
-	// Switch std::filesystem/stdfs to utf8.
+	// Switch boost::filesystem to utf8.
     try {
         boost::nowide::nowide_filesystem();
     } catch (const std::runtime_error& ex) {
@@ -1229,7 +1230,6 @@ int CLI::run(int argc, char **argv)
             false;
 #else
             // On Unix systems, the prusa-slicer binary may be symlinked to give the application a different meaning.
-            // boost filesystem is no longer an included file: this code will not work --v
             boost::algorithm::iends_with(boost::filesystem::path(argv[0]).filename().string(), "gcodeviewer");
 #endif // _WIN32*/
 
